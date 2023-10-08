f99609dc8d9c8196093cccae09ab4951814bca3d
thomas <thomasashraf94@gamil.com>
2023-10-08 08:21:02 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *
+ *Description: print digits from 1 to 9.
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	char x = 0;
+
+	while (x < 10)
+	{
+		putchar(x  + '0');
+			x++;
+	}
+	putchar('\n');
+	return (0);
+}
