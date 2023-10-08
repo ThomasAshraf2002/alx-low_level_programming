081659753e99a8a0e53b4e7e197938272d96331d
thomas <thomasashraf94@gamil.com>
2023-10-08 11:21:28 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: print single digit numbers
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (0 <= 9)
+	{
+		putchar(x + 48);
+
+		if (x != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
+
