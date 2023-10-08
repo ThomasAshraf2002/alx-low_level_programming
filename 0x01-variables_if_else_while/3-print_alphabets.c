618756970ebee0fa5cbd61d884cef9158437d51c
thomas <thomasashraf94@gamil.com>
2023-10-08 07:46:38 -0700

    a

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio>
+/**
+ *main - Entry point
+ *
+ *description: print alphabet in lowercase then in uppercase
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch1 = 'a';
+	char ch2 = 'A';
+
+	while (ch1 <= z)
+	{
+		putchar(ch1);
+		ch1++;
+	}
+	while (ch2 <= 'Z')
+	{
+		putchar(ch2);
+			ch2++;
+	}
+	putchar(\n);
+
+	return (0);
+
+
+
+}
+
