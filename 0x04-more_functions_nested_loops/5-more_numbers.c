9f77898b39f4e4f890f2c74dc53f2d2b3e2b849c
thomas <thomasashraf94@gamil.com>
2023-10-12 10:17:47 -0700

    a

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - print 10 times from 0 to 14
+ * Only use putchar twice
+ * Retuen: Always 0 (success)
+ *
+ */
+void more_numbers(void)
+{
+	int num;
+
+	if (num < 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i + 48)
+		}
+		_putchar('\n');
+	}
+}
