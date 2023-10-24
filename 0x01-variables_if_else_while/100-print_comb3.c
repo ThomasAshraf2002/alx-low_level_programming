a052ad18e09e68be246ba827c713f64005a655bb
thomas <thomasashraf94@gamil.com>
2023-10-24 04:32:37 -0700

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Descreption: prints all possible different combinations of two digits.
+ *
+ *Return: Always 0 (success)
+ */
+int main()
+{
+	int x,y;
+
+	for(x = 0;x < 10;x++)
+	{
+		for(y = 0 + 1;y < 10;y++)
+		{
+			putchar(x);
+			putchar(y);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
