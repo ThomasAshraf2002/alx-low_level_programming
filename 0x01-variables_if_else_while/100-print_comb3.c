f289ead520245afcea856a05f70170b997b09909
thomas <thomasashraf94@gamil.com>
2023-10-24 04:55:51 -0700

    a

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,19 +8,23 @@
  */
 #include <stdio.h>
 
-int main() {
-  int i = 0, j = 1;
-  while (i < 10) {
-    while (j < 10) {
-      putchar('0' + i);
-      putchar('0' + j);
-      putchar(',');
-      putchar(' ');
-      j++;
-    }
-    i++;
-    j = i + 1;
-  }
-  putchar('\n');
-  return 0;
+int main(void)
+{
+	int i = 0, j = 1;
+
+	while (i < 10)
+	{
+		while (j < 10)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			putchar(',');
+			putchar(' ');
+			j++;
+		}
+		i++;
+		j = i + 1;
+	}
+	putchar('\n');
+	return (0);
 }
