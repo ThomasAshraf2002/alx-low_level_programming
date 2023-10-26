79680e7ffac405d4185f8e6095dc9c0982680c84
thomas <thomasashraf94@gamil.com>
2023-10-25 18:58:41 -0700

    a

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,12 +7,11 @@
  */
 int factorial(int n)
 {
-	if (n == 1)
+	if (n < 0)
 	{
-		return (n);
+		return (-1);
 	}
-	else
-	{
-		return (n * factorial(n - 1));
-	}
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
 }
